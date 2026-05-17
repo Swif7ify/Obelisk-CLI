c1caff683d5105339a30a5b72498a4fdc8a4360b
Swif7ify <earl.coding@gmail.com>
2026-05-17 09:52:28 +0800

    feat: update Chocolatey install script to download the latest Obelisk CLI executable

--- packages/chocolatey/tools/chocolateyinstall.ps1
+++ packages/chocolatey/tools/chocolateyinstall.ps1
@@ -4,33 +4,15 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'obelisk-cli'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/Swif7ify/Obelisk-CLI/releases/download/v0.1.0/obelisk-windows-amd64.exe'
+$url64 = 'https://github.com/Swif7ify/Obelisk-CLI/releases/download/v1.1.0/obelisk.exe'
+$exePath = Join-Path $toolsDir 'obelisk.exe'
 
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url64bit      = $url64
-  
-  softwareName  = 'Obelisk CLI*'
-  
-  checksum64    = ''  # Will be filled during package creation
-  checksumType64= 'sha256'
-  
-  silentArgs    = ""
-  validExitCodes= @(0)
-}
-
-# Download and install
-Install-ChocolateyPackage @packageArgs
-
-# Rename to obelisk.exe
-$exePath = Join-Path $toolsDir 'obelisk-windows-amd64.exe'
-$targetPath = Join-Path $toolsDir 'obelisk.exe'
-
-if (Test-Path $exePath) {
-  Rename-Item -Path $exePath -NewName 'obelisk.exe' -Force
-}
+# Download the portable executable directly into the tools directory
+# Chocolatey will automatically create a shim for any .exe placed in this folder
+Get-ChocolateyWebFile -PackageName $packageName `
+                      -FileFullPath $exePath `
+                      -Url64bit $url64 `
+                      -Url $url64
 
 Write-Host "Obelisk CLI installed successfully!" -ForegroundColor Green
 Write-Host ""
